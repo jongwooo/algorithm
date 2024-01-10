4be543d3e0d92a1294fc5320b5f9b0badfb1d08a
Jongwoo Han <jongwooo.han@gmail.com>
2024-01-11 01:40:38 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+point = int(input())
+if 90 <= point <= 100:
+    print("A")
+elif 80 <= point <= 89:
+    print("B")
+elif 70 <= point <= 79:
+    print("C")
+elif 60 <= point <= 69:
+    print("D")
+else:
+    print("F")
