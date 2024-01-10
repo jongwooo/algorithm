412216d644f726a1906774fc38f344835dbe76c2
Jongwoo Han <jongwooo.han@gmail.com>
2024-01-11 02:01:29 +0900

    [Bronze III] Title: ACM 호텔, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,9 @@
+t = int(input())
+for _ in range(t):
+    h, _, n = map(int, input().split())
+    floor = n % h
+    room = n // h + 1
+    if floor == 0:
+        room -= 1
+        floor = h
+    print(floor * 100 + room)
