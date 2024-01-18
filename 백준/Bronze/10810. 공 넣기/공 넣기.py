c0388218e7f71e5aa4ed8e0a7bebe9ab36a454d2
Jongwoo Han <jongwooo.han@gmail.com>
2024-01-19 02:20:30 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+basket = [0] * n
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    for n in range(i, j + 1):
+        basket[n - 1] = k
+print(*basket)
