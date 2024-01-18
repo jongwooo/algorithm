4087b66bee3c8a32fcf5da39450d4ea7da58574b
Jongwoo Han <jongwooo.han@gmail.com>
2024-01-19 01:38:34 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,11 @@
+a, b, c = sorted(map(int, input().split()))
+if a == b == c:
+    print(10000 + 1000 * a)
+elif a == b:
+    print(1000 + 100 * a)
+elif b == c:
+    print(1000 + 100 * b)
+elif c == a:
+    print(1000 + 100 * c)
+else:
+    print(100 * c)
