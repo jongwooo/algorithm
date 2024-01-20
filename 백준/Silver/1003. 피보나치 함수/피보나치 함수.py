c066269b0505d5bb98dae670922faa5c5c5f8ecb
Jongwoo Han <jongwooo.han@gmail.com>
2024-01-20 19:00:48 +0900

    [Silver III] Title: 피보나치 함수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,10 @@
+dp = [[]] * 41
+dp[0] = [1, 0]
+dp[1] = [0, 1]
+dp[2] = [1, 1]
+for i in range(3, 41):
+    dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]]
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(*dp[n])
