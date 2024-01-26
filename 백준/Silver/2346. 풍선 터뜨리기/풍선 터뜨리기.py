a7e493a7c745bcf3349386808924d6e6405cf008
Jongwoo Han <jongwooo.han@gmail.com>
2024-01-26 22:03:15 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+n = int(input())
+balloons = deque(enumerate(map(int, input().split()), start=1))
+exploded = []
+while balloons:
+    i, paper = balloons.popleft()
+    exploded.append(i)
+    balloons.rotate(-paper + 1 if paper > 0 else -paper)
+print(' '.join(map(str, exploded)))
