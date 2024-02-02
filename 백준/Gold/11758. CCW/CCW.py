f0ca5e24cfc0b59d468d92f1066b9eca15af60b9
Jongwoo Han <jongwooo.han@gmail.com>
2024-02-02 13:16:46 +0900

    [Gold V] Title: CCW, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.py
@@ -0,0 +1,11 @@
+import sys
+
+x1, y1 = map(int, sys.stdin.readline().split())
+x2, y2 = map(int, sys.stdin.readline().split())
+x3, y3 = map(int, sys.stdin.readline().split())
+if (x1 * y2 - x2 * y1 + x2 * y3 - x3 * y2 + x3 * y1 - x1 * y3) / 2 > 0:
+    print(1)
+elif (x1 * y2 - x2 * y1 + x2 * y3 - x3 * y2 + x3 * y1 - x1 * y3) / 2 < 0:
+    print(-1)
+else:
+    print(0)
