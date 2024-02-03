a8837f5d3d8ef19e2d56ca5ff3f8b0197851ce0b
Jongwoo Han <jongwooo.han@gmail.com>
2024-02-03 15:50:57 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,14 @@
+import sys
+
+for _ in range(3):
+    yut = sys.stdin.readline()
+    if yut.count('0') == 1:
+        print('A')
+    elif yut.count('0') == 2:
+        print('B')
+    elif yut.count('0') == 3:
+        print('C')
+    elif yut.count('0') == 4:
+        print('D')
+    else:
+        print('E')
