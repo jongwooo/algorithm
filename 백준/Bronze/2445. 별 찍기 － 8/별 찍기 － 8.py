3d5535aed2594afa84833fd9fde77aa00a3c7443
Jongwoo Han <jongwooo.han@gmail.com>
2024-02-04 15:03:25 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,7 @@
+import sys
+
+n = int(sys.stdin.readline())
+for i in range(1, n + 1):
+    print('*' * i + ' ' * 2 * (n - i) + '*' * i)
+for i in range(1, n):
+    print('*' * (n - i) + ' ' * 2 * i + '*' * (n - i))
