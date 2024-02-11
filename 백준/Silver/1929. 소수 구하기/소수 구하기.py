c732d26fb2ea79f2bc8d71ea68c26de69d97b8d3
Jongwoo Han <jongwooo.han@gmail.com>
2024-02-11 19:40:40 +0900

    [Silver III] Title: 소수 구하기, Time: 3164 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,17 @@
+import math
+import sys
+
+
+def is_prime_number(x):
+    if x == 1:
+        return False
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+m, n = map(int, sys.stdin.readline().split())
+for i in range(m, n + 1):
+    if is_prime_number(i):
+        print(i)
