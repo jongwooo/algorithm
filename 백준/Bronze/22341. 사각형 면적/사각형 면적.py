aa3335cb893cd1a58de7d58ff8bfe5f79322c80c
Jongwoo Han <jongwooo.han@gmail.com>
2024-02-12 22:42:54 +0900

    [Bronze II] Title: 사각형 면적, Time: 48 ms, Memory: 31120 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22341. 사각형 면적/사각형 면적.py
@@ -0,0 +1,13 @@
+import sys
+
+n, c = map(int, sys.stdin.readline().split())
+a, b = n, n
+for _ in range(c):
+    x, y = map(int, sys.stdin.readline().split())
+    if a <= x or b <= y:
+        continue
+    if a * y < x * b:
+        a = x
+    else:
+        b = y
+print(a * b)
