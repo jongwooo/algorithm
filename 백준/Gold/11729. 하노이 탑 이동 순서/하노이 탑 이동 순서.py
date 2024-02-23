8af09e60567c537095ebaf9d1a2923a40c72f506
Jongwoo Han <jongwooo.han@gmail.com>
2024-02-23 11:12:28 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 872 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def hanoi_tower(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+    hanoi_tower(n - 1, start, 6 - start - end)
+    print(start, end)
+    hanoi_tower(n - 1, 6 - start - end, end)
+
+
+n = int(sys.stdin.readline())
+print(2 ** n - 1)
+hanoi_tower(n, 1, 3)
