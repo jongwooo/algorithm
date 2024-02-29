89b154f0da087a7956507065b75ff1333a428f62
Jongwoo Han <jongwooo.han@gmail.com>
2024-02-29 11:18:13 +0900

    [Silver III] Title: 01타일, Time: 364 ms, Memory: 69652 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+dp = [0] * (n + 1)
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+elif n >= 3:
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 15_746
+    print(dp[n])
