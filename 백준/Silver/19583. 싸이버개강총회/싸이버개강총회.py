9129a3495f283b947e815f6188275d13b2d483ac
Jongwoo Han <jongwooo.han@gmail.com>
2024-03-04 18:26:27 +0900

    [Silver II] Title: 싸이버개강총회, Time: 156 ms, Memory: 43412 KB -BaekjoonHub

---
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
@@ -0,0 +1,20 @@
+import sys
+
+start, end, stream = sys.stdin.readline().split()
+start = int(start[:2] + start[3:])
+end = int(end[:2] + end[3:])
+stream = int(stream[:2] + stream[3:])
+attend = set()
+cnt = 0
+while True:
+    try:
+        time, name = sys.stdin.readline().split()
+        time = int(time[:2] + time[3:])
+        if time <= start:
+            attend.add(name)
+        elif end <= time <= stream and name in attend:
+            attend.remove(name)
+            cnt += 1
+    except:
+        break
+print(cnt)
