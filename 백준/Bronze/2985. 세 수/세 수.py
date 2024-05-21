e53c06b68bbc3b73a3193596795cbf88107ab8ef
Jongwoo Han <jongwooo.han@gmail.com>
2024-05-22 08:30:32 +0900

    [Bronze III] Title: 세 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.py
@@ -0,0 +1,19 @@
+import sys
+
+a, b, c = map(int, sys.stdin.readline().split())
+if a + b == c:
+    print(f'{a}+{b}={c}')
+elif a - b == c:
+    print(f'{a}-{b}={c}')
+elif a * b == c:
+    print(f'{a}*{b}={c}')
+elif a // b == c:
+    print(f'{a}/{b}={c}')
+elif a == b + c:
+    print(f'{a}={b}+{c}')
+elif a == b - c:
+    print(f'{a}={b}-{c}')
+elif a == b * c:
+    print(f'{a}={b}*{c}')
+elif a == b // c:
+    print(f'{a}={b}/{c}')
