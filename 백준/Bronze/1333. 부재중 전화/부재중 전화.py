53aed7a82b6800d83268272ded4aa4e159d65a6a
Jongwoo Han <jongwooo.han@gmail.com>
2024-05-31 10:15:44 +0900

    [Bronze II] Title: 부재중 전화, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/1333. 부재중 전화/부재중 전화.py
@@ -0,0 +1,14 @@
+import sys
+
+N, L, D = map(int, sys.stdin.readline().split())
+total = N * L + (N - 1) * 5
+song = [False] * total
+for i in range(0, total, L + 5):
+    for j in range(i, i + L):
+        song[j] = True
+for i in range(0, total, D):
+    if not song[i]:
+        print(i)
+        break
+else:
+    print(i + D)
