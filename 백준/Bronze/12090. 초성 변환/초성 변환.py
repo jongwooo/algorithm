ed35bfd498d36566ea6b9d6a7ca3e49a407768f7
Jongwoo Han <jongwooo.han@gmail.com>
2024-06-01 21:43:58 +0900

    [Bronze I] Title: 초성 변환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/12090. 초성 변환/초성 변환.py
@@ -0,0 +1,7 @@
+import sys
+
+first_consonant_letters = (
+    'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ')
+S = sys.stdin.readline().rstrip()
+for s in S:
+    print(first_consonant_letters[(ord(s) - ord('가')) // 588], end='')
