777ca2e6bdd563b738d00b494a49077c31a4169f
Jongwoo Han <jongwooo.han@gmail.com>
2024-07-17 10:34:07 +0900

    [Gold II] Title: Fibonacci Sequence, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/13075. Fibonacci Sequence/Fibonacci Sequence.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def mul(a, b):
+    result = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            num = 0
+            for k in range(2):
+                num += a[i][k] * b[k][j]
+            result[i][j] = num % 1_000_000_000
+    return result
+
+
+def power(b):
+    fib = [[1, 1], [1, 0]]
+    if b == 1:
+        return fib
+    x = power(b // 2)
+    if b % 2 == 0:
+        return mul(x, x)
+    return mul(mul(x, x), fib)
+
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    x = int(sys.stdin.readline())
+    print(power(x)[1][0])
