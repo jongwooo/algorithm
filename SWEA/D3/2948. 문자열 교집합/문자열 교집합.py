4562b333574b01f1c2a32a1d15fa38cf1c958f70
Jongwoo Han <jongwooo.han@gmail.com>
2024-07-30 17:24:02 +0900

    [D3] Title: 문자열 교집합, Time: 2,539 ms, Memory: 129,780 KB -BaekjoonHub

--- SWEA/D3/2948. 문자열 교집합/문자열 교집합.py
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.py
@@ -1,6 +1,17 @@
 t = int(input())
 for test_case in range(1, t + 1):
     n, m = map(int, input().split())
-    s1 = set(input().split())
-    s2 = set(input().split())
-    print(f'#{test_case} {len(s1 & s2)}')
+    s1 = sorted(list(input().split()))
+    s2 = sorted(list(input().split()))
+    p1, p2 = 0, 0
+    cnt = 0
+    while p1 < n and p2 < m:
+        if s1[p1] == s2[p2]:
+            cnt += 1
+            p1 += 1
+            p2 += 1
+        elif s1[p1] < s2[p2]:
+            p1 += 1
+        else:
+            p2 += 1
+    print(f'#{test_case} {cnt}')
