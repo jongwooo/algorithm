827a5decd04c4c4a98227ee25e6059301810459f
Jongwoo Han <jongwooo.han@gmail.com>
2024-07-30 17:30:50 +0900

    [D3] Title: 문자열 교집합, Time: 822 ms, Memory: 135,812 KB -BaekjoonHub

--- SWEA/D3/2948. 문자열 교집합/문자열 교집합.java
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.java
@@ -1,56 +1,41 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-
-	static int N, M;
-	static String[] s1, s2;
-
-	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("input.txt"));
-		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		final StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		final int T = Integer.parseInt(br.readLine());
-		for (int testCase = 1; testCase <= T; testCase++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			st = new StringTokenizer(br.readLine());
-			s1 = new String[N];
-			for (int i = 0; i < N; i++) {
-				s1[i] = st.nextToken();
-			}
-			st = new StringTokenizer(br.readLine());
-			s2 = new String[M];
-			for (int i = 0; i < M; i++) {
-				s2[i] = st.nextToken();
-			}
-			Arrays.sort(s1);
-			Arrays.sort(s2);
-			int result = 0;
-			int p1 = 0;
-			int p2 = 0;
-			while (p1 < N && p2 < M) {
-				final int compare = s1[p1].compareTo(s2[p2]);
-				if (compare == 0) {
-					result++;
-					p1++;
-					p2++;
-				} else if (compare < 0) {
-					p1++;
-				} else if (compare > 0) {
-					p2++;
-				}
-			}
-			sb.append("#")
-					.append(testCase)
-					.append(" ")
-					.append(result)
-					.append("\n");
-		}
-		System.out.println(sb);
-	}
+ 
+    static int N, M;
+ 
+    public static void main(String[] args) throws Exception {
+//      System.setIn(new FileInputStream("input.txt"));
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        final int T = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= T; testCase++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            final Set<String> strSet = new HashSet<>();
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                strSet.add(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            int result = 0;
+            for (int i = 0; i < M; i++) {
+                if (strSet.contains(st.nextToken())) {
+                    result++;
+                }
+            }
+            sb.append("#")
+                    .append(testCase)
+                    .append(" ")
+                    .append(result)
+                    .append("\n");
+        }
+        System.out.println(sb);
+    }
 }
