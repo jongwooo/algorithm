e5735cab593be4eec02dff92422cd3a014ded4fd
Jongwoo Han <jongwooo.han@gmail.com>
2024-08-09 09:07:11 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,11 @@
+import sys
+
+s = sys.stdin.readline().rstrip()
+if s == 'SONGDO':
+    print('HIGHSCHOOL')
+elif s == 'CODE':
+    print('MASTER')
+elif s == '2023':
+    print('0611')
+elif s == 'ALGORITHM':
+    print('CONTEST')
