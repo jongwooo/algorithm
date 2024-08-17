d912ecf67740aa122586416f800dfe73422a612c
Jongwoo Han <jongwooo.han@gmail.com>
2024-08-17 15:57:36 +0900

    [D3] Title: 이진수 표현, Time: 150 ms, Memory: 30,604 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		final int T = Integer.parseInt(br.readLine());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			st = new StringTokenizer(br.readLine());
+			final int N = Integer.parseInt(st.nextToken());
+			final int M = Integer.parseInt(st.nextToken());
+			final int bits = (1 << N) - 1;
+			String result = "OFF";
+			if ((M & bits) == bits) {
+				result = "ON";
+			}
+			sb.append("#")
+					.append(testCase)
+					.append(" ")
+					.append(result)
+					.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
