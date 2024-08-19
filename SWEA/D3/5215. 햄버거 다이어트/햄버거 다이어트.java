d2fd83dd9f4dd612a413a50ff2577fa988df0d71
Jongwoo Han <jongwooo.han@gmail.com>
2024-08-19 18:17:14 +0900

    [D3] Title: 햄버거 다이어트, Time: 796 ms, Memory: 21,380 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -7,6 +7,7 @@ public class Solution {
 	static int N, L;
 	static int[] points;
 	static int[] calories;
+	static int[] array;
 	static int result;
 
 	public static void main(String[] args) throws Exception {
@@ -26,21 +27,12 @@ public class Solution {
 				points[i] = Integer.parseInt(st.nextToken());
 				calories[i] = Integer.parseInt(st.nextToken());
 			}
-			for (int i = 0; i < 1 << N; i++) {
-				int pointSum = 0;
-				int calorieSum = 0;
-				for (int j = 0; j < N; j++) {
-					if ((i & 1 << j) != 0) {
-						pointSum += points[j];
-						calorieSum += calories[j];
-					}
+			for (int i = 1; i <= N; i++) {
+				array = new int[N];
+				for (int j = array.length - 1; j > array.length - 1 - i; --j) {
+					array[j] = 1;
 				}
-				if (calorieSum > L) {
-					continue;
-				}
-				if (result < pointSum) {
-					result = pointSum;
-				}
+				combine();
 			}
 			sb.append("#")
 					.append(testCase)
@@ -49,5 +41,51 @@ public class Solution {
 					.append("\n");
 		}
 		System.out.println(sb);
+	}
+
+	private static void combine() {
+		do {
+			int point = 0;
+			int calorie = 0;
+			for (int i = 0; i < N; i++) {
+				if (array[i] == 1) {
+					point += points[i];
+					calorie += calories[i];
+				}
+			}
+			if (calorie > L) {
+				continue;
+			}
+			if (result < point) {
+				result = point;
+			}
+		} while (nextPermutation());
+	}
+
+	private static boolean nextPermutation() {
+		int length = array.length;
+		int top = length - 1;
+		while (top > 0 && array[top - 1] >= array[top]) {
+			top--;
+		}
+		if (top == 0) {
+			return false;
+		}
+		int right = length - 1;
+		while (array[top - 1] >= array[right]) {
+			right--;
+		}
+		swap(top - 1, right);
+		right = length - 1;
+		while (top < right) {
+			swap(top++, right--);
+		}
+		return true;
+	}
+
+	private static void swap(final int index1, final int index2) {
+		final int temp = array[index1];
+		array[index1] = array[index2];
+		array[index2] = temp;
 	}
 }
