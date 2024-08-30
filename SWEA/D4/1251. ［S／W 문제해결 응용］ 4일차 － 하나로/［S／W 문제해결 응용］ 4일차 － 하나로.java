f31ec2b31bf1d0d8d572e5346473c0d6616fabd9
Jongwoo Han <jongwooo.han@gmail.com>
2024-08-30 09:43:50 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 375 ms, Memory: 98,684 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,18 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 
 	static int N;
 	static long[] X, Y;
-	static int[] parent;
 	static double E;
-	static Edge[] edges;
+	static List<Edge>[] edges;
+	static long result;
 
 	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("input.txt"));
 		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		final StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
@@ -30,26 +31,17 @@ public class Solution {
 				Y[i] = Long.parseLong(st.nextToken());
 			}
 			E = Double.parseDouble(br.readLine());
-			parent = new int[N];
-			for (int i = 0; i < N; i++) {
-				parent[i] = i;
-			}
-			edges = new Edge[N * (N - 1) / 2];
+			edges = new List[N];
 			int index = 0;
-			for (int i = 0; i < N - 1; i++) {
-				for (int j = i + 1; j < N; j++) {
+			for (int i = 0; i < N; i++) {
+				edges[i] = new ArrayList<>();
+				for (int j = 0; j < N; j++) {
 					final long L = (X[i] - X[j]) * (X[i] - X[j]) + (Y[i] - Y[j]) * (Y[i] - Y[j]);
-					edges[index++] = new Edge(i, j, L);
+					edges[i].add(new Edge(j, L));
 				}
 			}
-			Arrays.sort(edges);
-			double result = 0.0;
-			for (final Edge edge : edges) {
-				if (find(edge.from) != find(edge.to)) {
-					union(edge.from, edge.to);
-					result += edge.cost;
-				}
-			}
+			result = 0;
+			prim();
 			sb.append("#")
 					.append(testCase)
 					.append(" ")
@@ -59,38 +51,43 @@ public class Solution {
 		System.out.println(sb);
 	}
 
-	private static int find(final int x) {
-		if (parent[x] == x) {
-			return x;
-		}
-		return parent[x] = find(parent[x]);
-	}
-
-	private static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if (a < b) {
-			parent[b] = a;
-		} else {
-			parent[a] = b;
+	private static void prim() {
+		final PriorityQueue<Edge> pq = new PriorityQueue<>();
+		final boolean[] visited = new boolean[N];
+		int count = 0;
+		pq.add(new Edge(0, 0));
+		while (!pq.isEmpty()) {
+			final Edge edge = pq.poll();
+			if (visited[edge.v]) {
+				continue;
+			}
+			result += edge.weight;
+			visited[edge.v] = true;
+			if (++count == N) {
+				break;
+			}
+			for (final Edge next : edges[edge.v]) {
+				if (!visited[next.v]) {
+					pq.add(next);
+				}
+			}
 		}
 	}
 }
 
 class Edge implements Comparable<Edge> {
 
-	final int from, to;
-	final long cost;
+	final int v;
+	final long weight;
 
-	public Edge(final int from, final int to, final long cost) {
+	public Edge(final int v, final long weight) {
 		super();
-		this.from = from;
-		this.to = to;
-		this.cost = cost;
+		this.v = v;
+		this.weight = weight;
 	}
 
 	@Override
 	public int compareTo(final Edge other) {
-		return Long.compare(this.cost, other.cost);
+		return Long.compare(this.weight, other.weight);
 	}
 }
