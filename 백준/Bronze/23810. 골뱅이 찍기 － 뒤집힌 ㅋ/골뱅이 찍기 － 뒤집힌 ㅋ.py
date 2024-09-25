a4ceded87f0974e178cb0b8df6ee7547805eca12
Jongwoo Han <jongwooo.han@gmail.com>
2024-09-26 08:59:03 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+for _ in range(n):
+    print('@' * 5 * n)
+for _ in range(n):
+    print('@' * n)
+for _ in range(n):
+    print('@' * 5 * n)
+for _ in range(n):
+    print('@' * n)
+for _ in range(n):
+    print('@' * n)
