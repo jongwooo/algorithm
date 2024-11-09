ea1d54174a62dc76397ae118baa464b5a411080f
Jongwoo Han <jongwooo.han@gmail.com>
2024-11-09 16:06:26 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+for i in range(n):
+    print('@' * 5 * n)
+for i in range(n):
+    print('@' * n + ' ' * 3 * n + '@' * n)
+for i in range(n):
+    print('@' * n + ' ' * 3 * n + '@' * n)
+for i in range(n):
+    print('@' * n + ' ' * 3 * n + '@' * n)
+for i in range(n):
+    print('@' * 5 * n)
