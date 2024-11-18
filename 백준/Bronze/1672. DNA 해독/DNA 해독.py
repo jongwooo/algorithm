7528115e8d467c7fcef726c946657596e7b2beba
Jongwoo Han <jongwooo.han@gmail.com>
2024-11-18 17:12:49 +0900

    [Bronze I] Title: DNA 해독, Time: 452 ms, Memory: 39912 KB -BaekjoonHub

---
+++ 백준/Bronze/1672. DNA 해독/DNA 해독.py
@@ -0,0 +1,16 @@
+import sys
+
+dna = {"AA": "A", "AG": "C", "AC": "A", "AT": "G", "GA": "C", "GG": "G",
+       "GC": "T", "GT": "A", "CA": "A", "CG": "T", "CC": "C", "CT": "G",
+       "TA": "G", "TG": "A", "TC": "G", "TT": "T"}
+
+n = int(sys.stdin.readline())
+s = list(sys.stdin.readline().rstrip())
+while True:
+    if len(s) == 1:
+        break
+    ss = s[-2] + s[-1]
+    if ss in dna:
+        del s[-2:]
+        s.append(dna.get(ss))
+print(*s)
