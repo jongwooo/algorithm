b7adc7b5903c4446ea2c610ac0193abcc709be12
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-08 00:34:09 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b + c;
+}
