0b7cda6aa86d4d7ec252267d121326688a6ce1e8
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-07 15:27:57 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int A;
+    int B;
+
+    cin >> A;
+    cin >> B;
+
+    cout << A + B;
+    return 0;
+}
