debc92620fc69288a72394c47f8a4455b5c6cea0
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-08 00:25:28 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string id;
+    cin >> id;
+
+    cout << id << "??!" << endl;
+}
