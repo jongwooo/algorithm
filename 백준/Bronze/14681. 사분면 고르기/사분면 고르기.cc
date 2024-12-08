d40e909ec801b3a4dbf71fcc9ff9ff72fce8db9c
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-08 14:42:30 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int x, y;
+	cin >> x >> y;
+
+	if (x > 0) {
+		if (y > 0) {
+			cout << 1;
+		}
+		else {
+			cout << 4;
+		}
+	}
+	else {
+		if (y > 0) {
+			cout << 2;
+		}
+		else {
+			cout << 3;
+		}
+	}
+}
