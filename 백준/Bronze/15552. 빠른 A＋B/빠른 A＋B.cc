920a4a28b8f2bfbf92fc8e8519725f564ffb3b61
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-08 20:18:36 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t, a, b;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+}
