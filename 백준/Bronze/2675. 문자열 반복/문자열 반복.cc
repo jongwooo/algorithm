eaa6d367229c00eeed5ecf494124aa682e1227eb
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-10 08:58:23 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t, r;
+	string s;
+
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> r >> s;
+		for (int j = 0; j < s.length(); j++) {
+			for (int k = 0; k < r; k++) {
+				cout << s[j];
+			}
+		}
+		cout << '\n';
+	}
+}
