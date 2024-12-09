66cdaf07951403a153ac572237fed609cd9d35e4
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-09 16:00:52 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s;
+	cin >> s;
+
+	cout << s.length();
+}
