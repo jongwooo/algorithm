92a199bb15b4741dc6dd230523e83254f31a8ff7
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-10 10:57:32 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	cout << "         ,r\'\"7" << '\n';
+	cout << "r`-_   ,\'  ,/" << '\n';
+	cout << " \\. \". L_r\'" << '\n';
+	cout << "   `~\\/" << '\n';
+	cout << "      |" << '\n';
+	cout << "      |" << '\n';
+}
