7c09c6e9a352a4bf9a111102ecb067054ed39f22
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-11 17:57:02 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int n;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	cin >> n;
+	if (n == 1) return 0;
+	int i = 2;
+	while (n > 1) {
+		if (n % i == 0) {
+			cout << i << '\n';
+			n /= i;
+		}
+		else {
+			i++;
+		}
+	}
+}
