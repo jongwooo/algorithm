0f2cefe01eb97dd9b81110728b6fe3372b3987fe
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-11 13:30:13 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int n, num;
+int cnt = 0;
+
+bool isPrimeNumuber(int number) {
+	if (number == 1) return false;
+	for (int i = 2; i < number; i++) {
+		if (number % i == 0) return false;
+	}
+	return true;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (isPrimeNumuber(num)) cnt++;
+	}
+	cout << cnt;
+}
