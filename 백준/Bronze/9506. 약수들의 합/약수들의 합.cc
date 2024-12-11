a63901d733ebe9b49e8f13f5144e673371b32201
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-11 11:39:53 +0900

    [Bronze I] Title: 약수들의 합, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int arr[100000];
+int cnt, sum;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	while (true) {
+		cin >> n;
+		if (n == -1) break;
+		cnt = 0, sum = 0;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				arr[cnt++] = i;
+				sum += i;
+			}
+		}
+		cout << n;
+		if (n == sum) {
+			cout << " = ";
+			for (int i = 0; i < cnt; i++) {
+				cout << arr[i];
+				if (i != cnt - 1) cout << " + ";
+				arr[i] = 0;
+			}
+		}
+		else cout << " is NOT perfect.";
+		cout << '\n';
+	}
+}
