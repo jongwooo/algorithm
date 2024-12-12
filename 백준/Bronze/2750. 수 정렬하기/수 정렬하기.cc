a1b861031ad1fc932fd68d2dfffb6032ed99b03b
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-12 22:40:04 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>]
+using namespace std;
+
+int num[1000];
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
+	for (int i = 0; i < n; i++) {
+		cout << num[i] << '\n';
+	}
+}
