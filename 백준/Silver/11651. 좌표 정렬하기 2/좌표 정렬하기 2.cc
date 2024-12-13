d25f82db56b1ff266d48b61ac3e5bdf9561d5c6e
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-13 16:24:18 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef pair<int, int> pii;
+
+bool compare(pii p1, pii p2) {
+	if (p1.second == p2.second) return p1.first < p2.first;
+	return p1.second < p2.second;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	
+	int n, x, y;
+	vector<pair<int, int>> v;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+	sort(v.begin(), v.end(), compare);
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << ' ' << v[i].second << '\n';
+	}
+}
