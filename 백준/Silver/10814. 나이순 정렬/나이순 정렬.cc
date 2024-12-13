6cfca9cfba1d376fadb1f85a6e0690dede00c01f
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-14 01:03:22 +0900

    [Silver V] Title: 나이순 정렬, Time: 40 ms, Memory: 7776 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,29 +1,29 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <algorithm>
 using namespace std;
 
-typedef pair<int, string> pis;
+struct Person {
+	int age;
+	string name;
+};
 
-bool compare(pis p1, pis p2) {
-	if (p1.first == p2.first) return false;
-	return p1.first < p2.first;
+bool compare(const Person& p1, const Person& p2) {
+	if (p1.age != p2.age) return p1.age < p2.age;
+	return false;
 }
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 
-	int n, age;
-	string name;
-	vector<pis> v;
+	int n;
 	cin >> n;
+	Person persons[100001];
 	for (int i = 0; i < n; i++) {
-		cin >> age >> name;
-		v.push_back({ age, name });
+		cin >> persons[i].age >> persons[i].name;
 	}
-	stable_sort(v.begin(), v.end(), compare);
+	stable_sort(persons, persons + n, compare);
 	for (int i = 0; i < n; i++) {
-		cout << v[i].first << ' ' << v[i].second << '\n';
+		cout << persons[i].age << ' ' << persons[i].name << '\n';
 	}
 }
