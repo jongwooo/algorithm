00098dc93fbd2606cf59824612f3ee35abb523aa
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-13 10:00:25 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	string s;
+	cin >> s;
+	sort(s.begin(), s.end(), greater<char>());
+	cout << s;
+}
