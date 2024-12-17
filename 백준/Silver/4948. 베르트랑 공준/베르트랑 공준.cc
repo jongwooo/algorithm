fc26acc9c05f69857823462fcf8de92633e923b8
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-18 01:00:57 +0900

    [Silver II] Title: 베르트랑 공준, Time: 36 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -2,24 +2,29 @@
 #include <cmath>
 using namespace std;
 
-bool isPrimeNumber(int x) {
-	if (x <= 1) return false;
-	for (int i = 2; i <= (int)sqrt(x); i++) {
-		if (x % i == 0) return false;
-	}
-	return true;
-}
+const int RANGE = 2 * 123'456;
+int nums[RANGE];
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 
+	fill_n(nums, RANGE, 1);
+	for (int i = 1; i <= RANGE; i++) {
+		if (i == 1) continue;
+		for (int j = 2; j <= (int)sqrt(i); j++) {
+			if (i % j == 0) {
+				nums[i] = 0;
+				break;
+			}
+		}
+	}
 	int n;
 	while (true) {
 		cin >> n;
 		if (n == 0) break;
 		int cnt = 0;
 		for (int i = n + 1; i <= 2 * n; i++) {
-			if (isPrimeNumber(i)) cnt++;
+			cnt += nums[i];
 		}
 		cout << cnt << '\n';
 	}
