79810d98fb3ab492af8e1ebb8111da324e436aa0
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-21 11:20:14 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int x) {
+	if (x == 0) return 1;
+	return x * factorial(x - 1);
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	cout << factorial(n);
+}
