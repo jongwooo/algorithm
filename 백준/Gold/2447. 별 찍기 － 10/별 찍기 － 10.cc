1b88726505234fdc6daa6dc5d5b185df29e4195f
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-23 20:17:43 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 204 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void star(int i, int j, int n) {
+	if ((i / n) % 3 == 1 && (j / n) % 3 == 1) cout << ' ';
+	else if (n / 3 == 0) cout << '*';
+	else star(i, j, n / 3);
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			star(i, j, n);
+		}
+		cout << '\n';
+	}
+}
