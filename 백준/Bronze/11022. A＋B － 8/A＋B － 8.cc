45a7a438a67c8a0c83f3896d5d7823dce955e468
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-26 11:42:39 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -1,16 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(0)->sync_with_stdio(0);
 
 	int t, a, b;
 	cin >> t;
-
 	for (int i = 1; i <= t; i++) {
 		cin >> a >> b;
-		cout << "Case #" << i << ": " << a << " + " << b <<  " = " << a + b << '\n';
+		cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << '\n';
 	}
 }
