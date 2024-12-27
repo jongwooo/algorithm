0e07faaad682987a42fa736743ea1effb3105454
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-27 10:04:29 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -1,25 +1,22 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(0)->sync_with_stdio(0);
 
 	int a, b, c;
 	cin >> a >> b >> c;
-
 	if (a != b && b != c && c != a) {
 		cout << max({ a, b, c }) * 100;
 	}
 	else if (a == b && b == c) {
-		cout << 10000 + a * 1000;
+		cout << 10'000 + a * 1'000;
 	}
 	else if (a == b || a == c) {
-		cout << 1000 + a * 100;
+		cout << 1'000 + a * 100;
 	}
 	else {
-		cout << 1000 + b * 100;
+		cout << 1'000 + b * 100;
 	}
 }
