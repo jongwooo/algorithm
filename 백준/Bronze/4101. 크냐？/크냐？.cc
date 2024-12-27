e137ce33987d466de5cbf584bc60d7135054752e
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-27 10:07:38 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int a, b;
+	while (true) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) break;
+		cout << (a > b ? "Yes" : "No") << '\n';
+	}
+}
