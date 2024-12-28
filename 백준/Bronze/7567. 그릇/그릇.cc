f5a5645b0e296472dd86abc7daebb7c3860a875b
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-28 12:47:35 +0900

    [Bronze II] Title: 그릇, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/7567. 그릇/그릇.cc
+++ 백준/Bronze/7567. 그릇/그릇.cc
@@ -1,18 +1,18 @@
 #include <iostream>
-#include <string>
+#define MAX 51
 using namespace std;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	
-	string s;
+	char s[MAX];
 	cin >> s;
 	int height = 0;
 	char cur = ' ';
-	for (char& c : s) {
-		if (cur == c) height += 5;
+	for (int i = 0; s[i] != '\0'; i++) {
+		if (cur == s[i]) height += 5;
 		else {
-			cur = c;
+			cur = s[i];
 			height += 10;
 		}
 	}
