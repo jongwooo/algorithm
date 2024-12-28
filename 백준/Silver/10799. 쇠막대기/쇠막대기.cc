d8472bd0cecc61a77fe19620c2ba309f953bfaef
Jongwoo Han <jongwooo.han@gmail.com>
2024-12-28 12:52:34 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+#define MAX 100'001
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	
+	char s[MAX];
+	cin >> s;
+	stack<char> st;
+	int cnt = 0;
+	for (int i = 0; s[i] != '\0'; i++) {
+		if (s[i] == '(') st.push(s[i]);
+		else if (i > 0 && s[i - 1] == '(') {
+			st.pop();
+			cnt += st.size();
+		}
+		else {
+			st.pop();
+			cnt++;
+		}
+	}
+	cout << cnt;
+}
