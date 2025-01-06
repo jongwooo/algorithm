f253a8dd42254ada5c85c5b22c43a75bcbc695eb
Jongwoo Han <jongwooo.han@gmail.com>
2025-01-06 22:14:35 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+}
