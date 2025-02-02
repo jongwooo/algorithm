c5fe9228a29e963d2bed39a073da9a15608a0f96
Jongwoo Han <jongwooo.han@gmail.com>
2025-02-02 11:35:40 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void dfs(int depth, int next);
+
+int nineDwarfs[9];
+int sevenDwarfs[7] = { 0, };
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+
+	for (int i = 0; i < 9; i++) {
+		cin >> nineDwarfs[i];
+	}
+	dfs(0, 0);
+}
+
+void dfs(int depth, int next) {
+	if (depth == 7) {
+		int sum = 0;
+		for (int dwarf : sevenDwarfs) {
+			sum += dwarf;
+		}
+		if (sum == 100) {
+			for (int dwarf : sevenDwarfs) {
+				cout << dwarf << '\n';
+			}
+			exit(0);
+		}
+		return;
+	}
+	for (int i = next; i < 9; i++) {
+		sevenDwarfs[depth] = nineDwarfs[i];
+		dfs(depth + 1, i + 1);
+	}
+}
