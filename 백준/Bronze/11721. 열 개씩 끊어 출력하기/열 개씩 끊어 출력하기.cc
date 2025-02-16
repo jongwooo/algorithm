2916f42d014356cdaf9f106c22560ae69909222b
Jongwoo Han <jongwooo.han@gmail.com>
2025-02-16 20:24:12 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	char s[101];
+	cin >> s;
+	for (int i = 0; s[i] != '\0'; i++) {
+		cout << s[i];
+		if (i != 0 && (i + 1) % 10 == 0) cout << '\n';
+	}
+}
