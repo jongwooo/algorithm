b2b9362339a1ed0448f90dc394f04c4e7f454249
Jongwoo Han <jongwooo.han@gmail.com>
2025-02-22 01:09:43 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -1,17 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(0)->sync_with_stdio(0);
 
 	int a, b, c;
 	cin >> a >> b >> c;
-
 	int m = a * 60 + b + c;
 	int h = (m / 60) % 24;
 	m %= 60;
-
-	cout << h << " " << m;
+	cout << h << ' ' << m;
 }
