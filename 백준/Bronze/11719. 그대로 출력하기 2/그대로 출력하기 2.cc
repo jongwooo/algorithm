4676be4b85d7b8a8218b0758190203af45ea3296
Jongwoo Han <jongwooo.han@gmail.com>
2025-02-24 00:55:55 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	string s;
+	while (getline(cin, s)) {
+		cout << s << '\n';
+	}
+}
