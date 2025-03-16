78530a75d219619fa82b79aae3b5e32371319451
Jongwoo Han <jongwooo.han@gmail.com>
2025-03-17 08:48:33 +0900

    [Silver I] Title: 그림, Time: 324 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -2,39 +2,36 @@ import sys
 from collections import deque
 
 
-def bfs(y, x):
-    global max_size
-    queue = deque([])
-    queue.append((y, x))
-    visited[y][x] = 1
-    size = 1
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+
+def bfs(x, y):
+    queue = deque([(x, y)])
+    visited[x][y] = 1
+    area = 0
     while queue:
-        y, x = queue.popleft()
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < n and 0 <= nx < m and visited[ny][nx] == -1:
-                queue.append((ny, nx))
-                visited[ny][nx] = 1
-                size += 1
-    max_size = max(max_size, size)
+        x, y = queue.popleft()
+        area += 1
+        for dx, dy in dirs:
+            nx = x + dx
+            ny = y + dy
+            if in_range(nx, ny) and not visited[nx][ny] and grid[nx][ny]:
+                queue.append((nx, ny))
+                visited[nx][ny] = 1
+    return area
 
 
+dirs = ((-1, 0), (0, 1), (1, 0), (0, -1))
 n, m = map(int, sys.stdin.readline().split())
-board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-visited = [[-1] * m for _ in range(n)]
-dy = [-1, 1, 0, 0]
-dx = [0, 0, 1, -1]
-for y in range(n):
-    for x in range(m):
-        if board[y][x] == 0:
-            visited[y][x] = 0
-cnt = 0
-max_size = 0
-for y in range(n):
-    for x in range(m):
-        if visited[y][x] == -1:
-            cnt += 1
-            bfs(y, x)
-print(cnt)
-print(max_size)
+grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+area_cnt = 0
+max_area = 0
+visited = [[0] * m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] and not visited[i][j]:
+            area_cnt += 1
+            max_area = max(max_area, bfs(i, j))
+print(area_cnt)
+print(max_area)
