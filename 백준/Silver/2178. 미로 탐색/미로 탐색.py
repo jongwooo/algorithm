72d4f944f1b625fc5d67cee763768dd88e9a7592
Jongwoo Han <jongwooo.han@gmail.com>
2025-03-18 12:23:52 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,25 +1,26 @@
 from collections import deque
 
-n, m = map(int, input().split())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input())))
-dy = [0, 0, -1, 1]
-dx = [-1, 1, 0, 0]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
 
 
-def bfs(y, x):
-    queue = deque([])
-    queue.append((y, x))
+def bfs():
+    queue = deque([(0, 0)])
+    visited = [[0] * m for _ in range(n)]
+    visited[0][0] = 1
     while queue:
-        y, x = queue.popleft()
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < n and 0 <= nx < m and graph[ny][nx] == 1:
-                queue.append((ny, nx))
-                graph[ny][nx] = graph[y][x] + 1
-    return graph[n - 1][m - 1]
+        x, y = queue.popleft()
+        for dx, dy in dirs:
+            nx = x + dx
+            ny = y + dy
+            if in_range(nx, ny) and not visited[nx][ny] and grid[nx][ny]:
+                queue.append((nx, ny))
+                visited[nx][ny] = visited[x][y] + 1
+    return visited[n - 1][m - 1]
 
 
-print(bfs(0, 0))
+dirs = ((-1, 0), (0, 1), (1, 0), (0, -1))
+n, m = map(int, input().split())
+grid = [list(map(int, input())) for _ in range(n)]
+print(bfs())
