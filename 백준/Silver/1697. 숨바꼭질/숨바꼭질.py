ad471e766799cc4885dda61dde57ca679a74506b
Jongwoo Han <jongwooo.han@gmail.com>
2025-03-24 08:45:14 +0900

    [Silver I] Title: 숨바꼭질, Time: 104 ms, Memory: 37328 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,22 +1,28 @@
 import sys
 from collections import deque
 
+
+MAX_N = 100_001
+
+
+def bfs(n):
+    visited = [0] * MAX_N
+    queue = deque([(n, 0)])
+    while queue:
+        x, cnt = queue.popleft()
+        if x == k:
+            return cnt
+        for f in func:
+            next_n = f(x)
+            if 0 <= next_n <= 100_000 and not visited[next_n]:
+                queue.append((next_n, cnt + 1))
+                visited[next_n] = 1
+
+
 func = (
     lambda x: x + 1,
     lambda x: x - 1,
     lambda x: 2 * x
 )
 n, k = map(int, sys.stdin.readline().split())
-visited = [False] * 100_001
-queue = deque([])
-queue.append((n, 0))
-while queue:
-    n, cnt = queue.popleft()
-    if n == k:
-        print(cnt)
-        break
-    for f in func:
-        next_n = f(n)
-        if 0 <= next_n <= 100_000 and not visited[next_n]:
-            queue.append((next_n, cnt + 1))
-            visited[next_n] = True
+print(bfs(n))
