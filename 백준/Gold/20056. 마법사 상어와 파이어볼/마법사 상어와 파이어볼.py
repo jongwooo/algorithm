c32cfce91d9904a0784703b65795f10ce0dec2c5
Jongwoo Han <jongwooo.han@gmail.com>
2025-09-02 14:32:36 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 392 ms, Memory: 35612 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
@@ -1,54 +1,57 @@
 import sys
+from collections import deque
 
 
-def move():
+def fireballs_move():
     while fireballs:
-        r, c, m, s, d = fireballs.pop()
-        nr = (r + dr[d] * s) % N
-        nc = (c + dc[d] * s) % N
-        board[nr][nc].append((m, s, d))
+        r, c, m, s, d = fireballs.popleft()
+        nr = (r + dr[d] * s) % n
+        nc = (c + dc[d] * s) % n
+        grid[nr][nc].append((m, s, d))
 
 
 def merge_and_split():
-    for i in range(N):
-        for j in range(N):
-            if len(board[i][j]) >= 2:
+    for i in range(n):
+        for j in range(n):
+            if len(grid[i][j]) >= 2:
                 nm, ns = 0, 0
                 odd_cnt, even_cnt = 0, 0
                 cnt = 0
-                while board[i][j]:
-                    m, s, d = board[i][j].pop()
+                while grid[i][j]:
+                    m, s, d = grid[i][j].pop()
                     nm += m
                     ns += s
-                    if d % 2:
-                        odd_cnt += 1
-                    else:
+                    if d % 2 == 0:
                         even_cnt += 1
+                    else:
+                        odd_cnt += 1
                     cnt += 1
                 if odd_cnt == cnt or even_cnt == cnt:
-                    nd = [0, 2, 4, 6]
+                    nd = (0, 2, 4, 6)
                 else:
-                    nd = [1, 3, 5, 7]
+                    nd = (1, 3, 5, 7)
                 if nm // 5:
                     for dd in nd:
                         fireballs.append((i, j, nm // 5, ns // cnt, dd))
-            elif len(board[i][j]) == 1:
-                m, s, d = board[i][j].pop()
+            elif len(grid[i][j]) == 1:
+                m, s, d = grid[i][j].pop()
                 fireballs.append((i, j, m, s, d))
 
 
-N, M, K = map(int, sys.stdin.readline().split())
-fireballs = []
-for _ in range(M):
-    r, c, m, s, d = map(int, input().split())
-    fireballs.append((r - 1, c - 1, m, s, d))
-board = [[[] for _ in range(N + 1)] for _ in range(N + 1)]
+
+
 dr = (-1, -1, 0, 1, 1, 1, 0, -1)
 dc = (0, 1, 1, 1, 0, -1, -1, -1)
-for _ in range(K):
-    move()
+n, m, k = map(int, sys.stdin.readline().split())
+grid = [[[] for _ in range(n)] for _ in range(n)]
+fireballs = deque([])
+for i in range(1, m + 1):
+    r, c, m, s, d = map(int, sys.stdin.readline().split())
+    fireballs.append((r - 1, c - 1, m, s, d))
+for _ in range(k):
+    fireballs_move()
     merge_and_split()
-sum_m = 0
+m_sum = 0
 for _, _, m, _, _ in fireballs:
-    sum_m += m
-print(sum_m)
+    m_sum += m
+print(m_sum)
