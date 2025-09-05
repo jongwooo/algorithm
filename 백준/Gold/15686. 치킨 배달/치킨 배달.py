f4b4c8ddfb0466a1d39dc24187531ae3eed2116f
Jongwoo Han <jongwooo.han@gmail.com>
2025-09-05 10:53:42 +0900

    [Gold V] Title: 치킨 배달, Time: 232 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,7 +1,5 @@
 import sys
 
-INF = int(1e9)
-
 
 def distance(r1, c1, r2, c2):
     return abs(r1 - r2) + abs(c1 - c2)
@@ -11,10 +9,10 @@ def dfs(idx, chosen):
     global min_dist
     if len(chosen) == m:
         dist = 0
-        for hx, hy in homes:
+        for hr, hc in homes:
             temp_dist = INF
-            for cx, cy in chosen:
-                temp_dist = min(temp_dist, distance(hx, hy, cx, cy))
+            for cr, cc in chosen:
+                temp_dist = min(temp_dist, distance(hr, hc, cr, cc))
             dist += temp_dist
         min_dist = min(min_dist, dist)
         return
@@ -22,15 +20,18 @@ def dfs(idx, chosen):
         dfs(i + 1, chosen + [chicken_restaurants[i]])
 
 
+INF = int(1e9)
+HOME = 1
+CHICKEN_RESTAURANT = 2
 n, m = map(int, sys.stdin.readline().split())
-grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 chicken_restaurants = []
 homes = []
 for i in range(n):
+    temp = list(map(int, sys.stdin.readline().split()))
     for j in range(n):
-        if grid[i][j] == 2:
+        if temp[j] == CHICKEN_RESTAURANT:
             chicken_restaurants.append((i, j))
-        elif grid[i][j] == 1:
+        elif temp[j] == HOME:
             homes.append((i, j))
 min_dist = INF
 dfs(0, [])
