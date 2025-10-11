3682d4d127a3f7da045c8478847407916af41b3a
Jongwoo Han <jongwooo.han@gmail.com>
2025-10-11 22:16:08 +0900

    [level 0] Title: 문자열 출력하기, Time: 2.86 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    string str;
+    cin >> str;
+    cout << str;
+    return 0;
+}
