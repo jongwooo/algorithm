6b51598059f7a6649cbe2791d1a54ac330986c87
Jongwoo Han <jongwooo.han@gmail.com>
2025-10-11 22:17:22 +0900

    [level 0] Title: a와 b 출력하기, Time: 3.22 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int a, b;
+    cin >> a >> b;
+    cout << "a = " << a << '\n';
+    cout << "b = " << b << '\n';
+    return 0;
+}
