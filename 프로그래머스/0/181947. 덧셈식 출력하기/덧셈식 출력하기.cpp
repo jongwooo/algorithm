41ba87efbfebc3e254f8ea3afc9533394354dab3
Jongwoo Han <jongwooo.han@gmail.com>
2025-10-12 13:46:00 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 2.72 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(0) -> sync_with_stdio(0);
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a + b << '\n';
+    return 0;
+}
