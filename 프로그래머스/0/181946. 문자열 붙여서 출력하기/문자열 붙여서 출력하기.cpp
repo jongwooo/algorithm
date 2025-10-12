f5536c47b28ab328032ed481e481e7200655c7c3
Jongwoo Han <jongwooo.han@gmail.com>
2025-10-13 00:38:44 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 2.66 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(0) -> sync_with_stdio(0);
+    string str1, str2;
+    cin >> str1 >> str2;
+    cout << str1 << str2;
+    return 0;
+}
