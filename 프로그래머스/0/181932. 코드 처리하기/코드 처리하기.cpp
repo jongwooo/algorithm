8287c0f581a6c143d10e60f2a3ca8512aa7cd37d
Jongwoo Han <jongwooo.han@gmail.com>
2025-10-14 11:38:29 +0900

    [level 0] Title: 코드 처리하기, Time: 0.41 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    int mode = 0;
+    string ret = "";
+    for (int idx = 0; idx < code.size(); idx++) {
+        if (mode == 0) {
+            if (code[idx] != '1') {
+                if (idx % 2 == 0) {
+                    ret += code[idx];
+                }
+            } else {
+                mode = 1 - mode;
+            }
+        } else {
+            if (code[idx] != '1') {
+                if (idx % 2 == 1) {
+                    ret += code[idx];
+                }
+            } else {
+                mode = 1 - mode;
+            }
+        }
+    }
+    if (ret.empty()) {
+        return "EMPTY";
+    }
+    return ret;
+}
