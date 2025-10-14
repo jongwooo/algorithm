5c0428cdc5d3b36ba9a1b2e8af251df8a481c921
Jongwoo Han <jongwooo.han@gmail.com>
2025-10-14 11:26:26 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    if(ineq == ">") {
+        if (eq == "=") {
+            return n >= m;
+        } else if(eq == "!") {
+            return n > m;
+        }
+    } else if (ineq == "<") {
+        if (eq == "=") {
+            return n <= m;
+        } else if(eq == "!") {
+            return n < m;
+        }
+    }
+}
