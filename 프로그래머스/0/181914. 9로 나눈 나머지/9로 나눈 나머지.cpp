961e7135e04e5ce4b0c20ac11e4ddf661cb5c7e5
Jongwoo Han <jongwooo.han@gmail.com>
2025-10-16 15:03:49 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.10 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+int solution(string number) {
+    int sum = 0;
+    for (int i = 0; i < number.size(); ++i) {
+        sum += number[i] - '0';
+    }
+    return sum % 9;
+}
